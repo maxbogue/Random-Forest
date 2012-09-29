c85ceb8287f6274b9f9a794fe631d81b24dc1b5d
Max Bogue <max@maxbogue.com>
2012-09-29 01:19:39 -0400

    Move some code around for better organization and reusability.

--- DecisionTree.hs
+++ DecisionTree.hs
@@ -28,9 +28,16 @@ getAttributes (Tree attr children) = foldl
     (Map.singleton attr (Map.keys children))
     (map getAttributes (Map.elems children))
 
-decide :: DecisionTree a -> Choices -> a
-decide (Decision a) _ = a
-decide (Tree attr children) choices = decide (children Map.! (choices Map.! attr)) choices
+decide :: Choices -> DecisionTree a -> a
+decide _ (Decision a) = a
+decide choices (Tree attr children) = decide choices (children Map.! (choices Map.! attr))
+
+mode [] = error "An empty list doesn't have a mode."
+mode ls = snd $ maximum $ map swap $ Map.assocs $ mode' ls where
+    mode' [] = Map.empty
+    mode' (k:ks) = Map.alter inc k $ mode' ks
+    inc Nothing = Just 0
+    inc (Just n) = Just $ n + 1
 
 randomElement :: [t] -> IO t
 randomElement ls = do
@@ -68,20 +75,15 @@ bestAttribute attrs samples = snd $ maximum $ map (\(a, vs) -> (informationGain 
         priorH (v:vs) = let samples' = filterSamples attr v samples in
             (fromIntegral $ length samples') / numSamples * entropy samples' + priorH vs
 
+generateDecisionTree :: Ord a => Attributes -> [Sample a] -> DecisionTree a
 generateDecisionTree attrs samples = case nub $ map snd samples of
     []  -> error "Can't generate a decision tree without any samples."
     [d] -> Decision d
     _   -> if Map.null attrs
         then Decision $ mode $ map snd samples
         else let best = bestAttribute attrs samples in
-            Tree best $ Map.fromList [(v, generateSubtree best v) | v <- attrs Map.! best]
-  where
-    generateSubtree best v = case filterSamples best v samples of
-        []       -> Decision $ mode $ map snd samples
-        samples' -> generateDecisionTree (Map.delete best attrs) samples'
-    mode [] = error "An empty list doesn't have a mode."
-    mode ls = snd $ maximum $ map swap $ Map.assocs $ mode' ls where
-        mode' [] = Map.empty
-        mode' (k:ks) = Map.alter inc k $ mode' ks
-        inc Nothing = Just 0
-        inc (Just n) = Just $ n + 1
+            Tree best $ Map.fromList [
+                (v, case filterSamples best v samples of
+                    []       -> Decision $ mode $ map snd samples
+                    samples' -> generateDecisionTree (Map.delete best attrs) samples')
+                | v <- attrs Map.! best]
